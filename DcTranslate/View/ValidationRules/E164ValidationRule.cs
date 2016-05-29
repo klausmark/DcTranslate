2545633920a5993174debede9370e8fde46b0f31
Klaus Mark <klaus@11k.dk>
2016-05-29 22:45:43 +0200

    Validation

---
+++ DcTranslate/View/ValidationRules/E164ValidationRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace DcTranslate.View.ValidationRules
+{
+    public class E164ValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string s = (string) value;
+            var match = Regex.Match(s, @"^\+\d+$");
+            if (match.Success) return ValidationResult.ValidResult;
+            return new ValidationResult(false, "Not valid E.164 number");
+        }
+    }
+}
---
+++ DcTranslate/View/ValidationRules/FromNumberValidationRule.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace DcTranslate.View.ValidationRules
+{
+    public class FromNumberValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string s = (string)value;
+            var match = Regex.Match(s, @"^18\d{4}$");
+            if (match.Success) return ValidationResult.ValidResult;
+            return new ValidationResult(false, "Not valid from number number");
+        }
+    }
+}
